36673de20c6c02d761bc5d98734f26788fb19c96
Neykuratick <neykuratick@yahoo.com>
2022-02-06 15:20:40 +0300

    timefix

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ logging.basicConfig(level=config.LOGGING_LEVEL)
 
 
 def main():
-    schedule.every(86400).seconds.do(drive)
-    schedule.run_all(delay_seconds=10)
+    # schedule.every(86400).seconds.do(drive)
+    schedule.every().day.at("15:01").do(drive)  # time is in UTC (UTC+3 15:20 -> 12:20 UTC)
+    # schedule.run_all(delay_seconds=10)
 
     while True:
         schedule.run_pending()
